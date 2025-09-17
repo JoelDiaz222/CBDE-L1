b54bf7a9d6d047a4b22911978e443ed4d5222aa9
Joel Díaz <joeldiazgamer999@gmail.com>
2025-09-17 21:26:45 +0200

    Swap C1_new_collection for C1

--- chroma/C1.py
+++ chroma/C1.py
@@ -3,31 +3,41 @@ from sentence_transformers import SentenceTransformer
 import time
 
 client = chromadb.PersistentClient()
-collection = client.get_collection(name="bookcorpus_sentences_cosine")
+source_collection = client.get_collection(name="bookcorpus_sentences_cosine")
 
-ids_and_docs = collection.get()
+ids_and_docs = source_collection.get()
 ids = ids_and_docs['ids']
 documents = ids_and_docs['documents']
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 print(f"Generating embeddings for {len(ids)} sentences...")
-
 embeddings = model.encode(documents).tolist()
 
-print(f"Storing embeddings...")
+try:
+    client.delete_collection(name="bookcorpus_embeddings")
+except:
+    pass
+
+embeddings_collection = client.create_collection(
+    name="bookcorpus_embeddings",
+    metadata = {"hnsw:space": "cosine"}
+)
+
+print("Storing embeddings in new collection...")
 
 batch_size = 5000
 
 start = time.time()
 for i in range(0, len(ids), batch_size):
     end_idx = min(i + batch_size, len(ids))
-    collection.update(
+    embeddings_collection.add(
         ids=ids[i:end_idx],
+        documents=documents[i:end_idx],
         embeddings=embeddings[i:end_idx]
     )
 end = time.time()
 
 total_time = (end - start) * 1000
-print(f"Embeddings stored for all {len(ids)} sentences in {total_time:.8f} ms")
+print(f"Embeddings stored in new collection in {total_time:.8f} ms")
 print(f"Average time per embedding: {total_time / len(ids):.8f} ms")
